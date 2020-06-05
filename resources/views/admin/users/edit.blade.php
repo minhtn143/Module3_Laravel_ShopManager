2bf4a64c3f444140114cf28ada7360713d28763e
minhtn143 <minhtn143@gmail.com>
2020-06-06 00:13:32 +0700

    edit user done

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -4,29 +4,71 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Edit User {{ $user->name }}</div>
+            <form action='{{ route('admin.users.update', $user)}}' method="POST">
+                @csrf
+                {{ method_field('PUT') }}
+                <div class="card">
+                    <div class="card-header">Edit User {{ $user->name }}</div>
 
-                <div class="card-body">
-                    <form action="{{ route('admin.users.update',$user) }}" method="POST">
-                        @csrf
-                        {{ method_field('PUT') }}
-                        @foreach ($roles as $role)
-                        <div class="form-checkbox">
-                            <input type="checkbox" name="roles[]" value="{{ $role->id }}" id="role-{{ $role->id }}"
-                            @if($user->hasRoles($role->name))
-                                checked
-                            @endif>
-                            <label for="role->{{ $role->id }}">{{ $role->name }}</label>
+                    <div class="card-body">
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-2 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="￼ use￼ user
+                            ￼Updater
+                            ￼Updateform-control @error('email') is-invalid @enderror" name="email"
+                                    value="{{ $user->email }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
                         </div>
-                        @endforeach
-                        <button type="submit" class="btn btn-primary">Update</button>
-                        <button type="button" class="btn btn-secondary" action="window.history.go(-1)">Back</button>
 
-                    </form>
+                        <div class="card-body">
+
+                            <div class="form-group row">
+                                <label for="name" class="col-md-2 col-form-label text-md-right">Name</label>
+
+                                <div class="col-md-6">
+                                    <input id='name' type="text"
+                                        class="form-control @error('email') is-invalid @enderror" name="name"
+                                        value="{{ $user->name }}" required autofocus>
+                                </div>
+                                @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="roles" class="col-md-2 col-form-label text-md-right">Roles</label>
+                            <div class="col-md-6">
+                                @foreach ($roles as $role)
+                                <div class="form-checkbox">
+                                    <input type="checkbox" name="roles[]" value="{{ $role->id }}"
+                                        id="role-{{ $role->id }}" @if($user->hasRoles($role->name))
+                                    checked
+                                    @endif>
+                                    <label for="role-{{ $role->id }}">{{ $role->name }}</label>
+                                </div>
+                                @endforeach
+                                </ </div> <button type="submit" class="btn btn-primary">Update</button>
+                                <button type="button" class="btn btn-secondary"
+                                    action="window.history.go(-1)">Back</button>
+                            </div>
+
+                        </div>
+                    </div>
+
                 </div>
-            </div>
+            </form>
         </div>
     </div>
-</div>
-@endsection
+    @endsection
